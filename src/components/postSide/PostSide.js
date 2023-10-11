a8aabba6ad881788dc5cf7d42604f30172477f7f
ashwinilakshmanan <ashwinilakshmanan1@gmail.com>
2023-10-11 12:39:04 +0530

    created the posts file

--- src/components/postSide/PostSide.js
+++ src/components/postSide/PostSide.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import PostShare from '../postShare/PostShare'
+import Posts from '../posts/Posts'
 
 function PostSide() {
     return (
         <>
             <div className='postSide'>
             <PostShare/>
+            <Posts/>
             </div>
         </>
     )
---
+++ src/components/posts/Posts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Posts() {
+  return (
+    <>
+    <div className='posts'>
+    posts
+    </div>
+      
+    </>
+  )
+}
+
+export default Posts
