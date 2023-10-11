581f69e8535444d0fdb5f604627baaf313af42f2
ashwinilakshmanan <ashwinilakshmanan1@gmail.com>
2023-10-11 12:35:24 +0530

    created the postshare option

---
+++ src/components/postSide/PostSide.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PostShare from '../postShare/PostShare'
+
+function PostSide() {
+    return (
+        <>
+            <div className='postSide'>
+            <PostShare/>
+            </div>
+        </>
+    )
+}
+
+export default PostSide
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -9,7 +9,7 @@ function Home() {
       <div className='Home'>
         <ProfileSide/>
         <div className='postSide'>
-          Post
+         
           <PostSide/>
         </div>
         <div className='rightSide'>Right side</div>
